0415c97853779b27c660a48247da20bde69f9495
JhopperJH <jumphopper.jh@gmail.com>
2022-04-07 14:58:01 +0700

    Update Mode.java

--- src/com/company/Mode.java
+++ src/com/company/Mode.java
@@ -8,4 +8,16 @@ public abstract class Mode {
 
     public abstract void setOperator();
     public abstract void  randomNum();
+
+    public String getOperator(){
+        return operator;
+    }
+
+    public int getNum1(){
+        return num1;
+    }
+
+    public int getNum2(){
+        return num2;
+    }
 }
