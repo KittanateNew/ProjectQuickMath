4531b57fa0e43c23af2367565766181aad66aa2a
KittanateNew <6434407623@student.chula.ac.th>
2022-04-09 12:38:25 +0700

    gg

---
+++ src/com/company/Menu.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+import javax.swing.plaf.IconUIResource;
+
+public class Menu extends JFrame{
+    private JPanel MainMenu;
+
+    public Menu(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(MainMenu);
+        this.setVisible(true);
+        this.pack();
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new Menu("Quick Math");
+        frame.setSize(650,450);
+    }
+}
