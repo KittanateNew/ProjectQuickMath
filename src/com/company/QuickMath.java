3e6bfcfc5cc490c28453c7f03a297ff3725c6152
KittanateNew <6434407623@student.chula.ac.th>
2022-04-09 22:39:07 +0700

    T - T make 2 Gui Sat 9

---
+++ src/com/company/QuickMath.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import javax.swing.*;
+
+public class QuickMath {
+    public static void main(String[] args){
+        JFrame frame = new Menu("Quick Math");
+        frame.setSize(850,650);
+    }
+}
